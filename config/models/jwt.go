282b6344e70ad83401cc120d92726f828feb3321
anthanhphan <an.thanhphan.work@gmail.com>
2024-12-01 18:23:01 +0700

    fix: naming convention jwt model

--- config/models/jwt.go
+++ config/models/jwt.go
@@ -1,6 +1,6 @@
 package models
 
-type JwtConfig struct {
+type Jwt struct {
 	PrivateKeyPath     string `yaml:"private_key_path" json:"private_key_path"`
 	PublicKeyPath      string `yaml:"public_key_path" json:"public_key_path"`
 	AccessTokenExpiry  int64  `yaml:"access_token_expiry" json:"access_token_expiry"`
